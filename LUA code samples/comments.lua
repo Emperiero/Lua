c3f757ec1af46a6e06386bf80e4dc7f1f77877ea
anthony <89143917+moronnnn@users.noreply.github.com>
2021-10-11 18:13:19 -0400

    Add files via upload

---
+++ LUA code samples/comments.lua
@@ -0,0 +1,18 @@
+-- making a comment in lua is simple, as you can see...
+
+--just type "--" and then what you need to comment.
+
+-- to write a comment with multiple lines, youll need to do "--[[ --]]"
+
+-- so that will look like this
+
+--[[ this 
+is
+a
+multi
+line
+comment
+in
+lua --]]
+
+-- note: the end to the comment doesnt have to be on the same line :) 
---
+++ LUA code samples/variables.lua
@@ -0,0 +1,27 @@
+--[[ this is going to be focused on variables, very simple concept
+but super important to understand when programming in any language lol --]]
+
+-- in lua theres lots of different ways of doing variables
+
+-- variable def
+
+local x, y 
+
+-- assign 
+
+x = 10
+y = 20
+
+-- printing the variable
+
+print("the value of x is: ", x)
+print("the value of y is: ", y)
+
+
+--[[ this is going to print out the number the variable is assigned too
+so it will end up looking like this --]]
+
+the value of x is: 10
+the value ox y is: 20
+
+-- pretty simple concept right?

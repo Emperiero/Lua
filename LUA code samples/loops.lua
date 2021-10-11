58ec66485d7df6c35c3c9fb8fb208d89df3316dd
anthony <89143917+moronnnn@users.noreply.github.com>
2021-10-11 18:34:50 -0400

    Add files via upload

---
+++ LUA code samples/loops.lua
@@ -0,0 +1,26 @@
+--[[ in LUA you have loops, just like every other language to exist
+you have your conditional loops --]]
+
+for loops
+-- executes a sequence of statements multiple times, or abbreviates the code that manages the loop variable 
+
+
+while loops
+-- repeats a statement or group of statements while already given a condition 
+-- it will test the condition BEFORE executing itself
+
+repeat until loop 
+--repeats the operation of group of statemens till a condition is met
+
+nested loop
+-- you can use one or more loop inside of any other loop
+
+break 
+-- breaks or terminates the loop
+
+example below 
+
+while ( true )
+do
+    print("this loop is going to run forever")
+end

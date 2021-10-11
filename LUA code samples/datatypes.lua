98a3cb4fca81e5fb06c3f3d162bc510331badc49
anthony <89143917+moronnnn@users.noreply.github.com>
2021-10-11 18:22:20 -0400

    Add files via upload

---
+++ LUA code samples/datatypes.lua
@@ -0,0 +1,35 @@
+--[[ this is probably one of the most IMPORTANT things 
+you will need to learn so PLEASE take this part to importance. --]]
+
+-- your data types are very important, heres a table listing them all
+
+nil
+--[[ nil is pretty much the value of nothing so when you have some data
+or you can have NO data meaning it will be a nil value, (or a value of nothing) --]]
+
+boolean 
+-- includes if a value is true or false, usually used as checking conditions 
+
+number 
+--self explanatory 
+
+string 
+-- shows an array of characters, youll learn about arrays later
+
+function 
+    --[[ represents something that is written in either LUA OR C
+    ur gonna notice how LUA and C have relation later --]]
+
+
+userdata 
+-- arbitray in c data (i barely dont fucking shit about this LMFAO)
+
+thread 
+-- Represents independent threads of execution and it is used to implement coroutines.
+
+table 
+--[[ this is a big one so be represent just abt everything
+including arrays, symbol tables, sets, records, graphs, trees etc etc 
+it can also implement associative arrays and can hold any value
+note: THE ONLY VALUE IT CANT HOLD IS NIL!!!! --]]
+

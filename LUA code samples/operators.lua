4089818a0efd26acf4d849f6d2d399a1616ed1e7
anthony <89143917+moronnnn@users.noreply.github.com>
2021-10-11 18:30:38 -0400

    Add files via upload

---
+++ LUA code samples/operators.lua
@@ -0,0 +1,52 @@
+--[[ this is a decently sized part of lua
+these are your math operators/operators in general --]] 
+
++ 
+-- addition 
+
+- 
+-- subtraction 
+
+*
+-- multiplication 
+
+/ 
+-- divide 
+
+-- these are your BASIC operators you can look more abt operators simply on the internet 
+
+-- here are your relational operators
+-- so lets just assume the value of x is 10 and the value of y is 20 
+
+== 
+-- checks if the value of multiple numbers are equal or not if yes then the condition is true
+
+~= 
+-- checks if the value of two numbers are equal or not, if the are NOT equal, the condition becomes true
+
+> 
+-- this one is self explanatory 
+
+< 
+-- same with this one
+
+>= 
+--[[ checks if the value of the left number is greater or equal to the value on the right 
+if yes, then the condition becomes true --]]
+
+<= 
+-- same thing with the last one JUST the opposite 
+
+logical operators 
+
+and 
+-- if both of the numbers are a non zero then the condition becomes true
+
+or 
+-- checks if any of the two numbers is a non zero, if so the condition is true
+
+not 
+-- checks if the logical state of its numbers if a condition is true, then the the logical NOT operator will make it false
+
+
+

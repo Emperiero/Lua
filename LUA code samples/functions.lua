32e42e3806103a8dc1e36dbce1b5313adef811ee
anthony <89143917+moronnnn@users.noreply.github.com>
2021-10-11 04:01:46 -0400

    Add files via upload

---
+++ LUA code samples/functions.lua
@@ -0,0 +1,32 @@
+-- here im going to be explaining functions in lua and how they work
+
+-- best way to define a function is by doing this
+
+function test()
+    function (testFunction)
+        print("this is  a test function")
+    end
+
+    --[[ this may look odd at first, but it does work
+    make sure when you end it you end it with --]]
+
+    test().testFunction()
+
+    --[[ another cool thing you can do is use this thing called
+    an object it will look more like this --]]
+
+    test = {
+        test0 = (function()
+            return 'testt'
+        end),
+        test1 = (function()
+            return 'testttt.'
+        end),
+        test2 = (function()
+            return 'testtttt'
+        end)
+    }
+
+    -- credit for this goes to stackoverflow in questions "function callin within lua"
+    
+    

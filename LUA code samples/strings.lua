166457a8c35aea47a9406e48941d9bed23d6d013
anthony <89143917+moronnnn@users.noreply.github.com>
2021-10-18 00:30:53 -0400

    Add files via upload

---
+++ LUA code samples/strings.lua
@@ -0,0 +1,16 @@
+-- this is going to be all about strings in lua, this is a pretty simple concept until you get to arrays so
+
+string1 = "hello"
+print("\"String 1\"",string1)
+
+-- second way to do strings is
+
+string2 = 'hello'
+print("String 2",string2)
+
+-- and last way is 
+
+string3 = [["Hello!!"]]
+print("String 3 ",string3)
+
+-- now again this is a simple explanation of strings in LUA to learn more i would suggest reading at "https://www.tutorialspoint.com/lua/lua_strings.htm"
